d6bd89bf15f370d89e9a0e11c19b032c01f644df
Carlos Villuendas <carlosvillu@gmail.com>
2018-05-21 21:10:56 +0200

    feat(literals): add enGB literals

--- src/literals/en-GB.js
+++ src/literals/en-GB.js
@@ -1,5 +1,23 @@
 module.exports = {
-  HOME_TITLE: 'Home',
-  LIST_TITLE: 'List',
-  DETAIL_TITLE: 'Detail'
+  HOME: 'Home',
+  ABOUT_US: 'About us',
+  CONTACT_US: 'Contact us',
+  HOURS: 'hours',
+  MINUTES: 'minutes',
+  SECONDS: 'seconds',
+  DESTINATION_ACCOUNT_TITLE: '1. Start by introducing your destination account',
+  DESTINATION_ACCOUNT_DESCRIPTION: 'Make sure you OWN this account.',
+  DESTINATION_ACCOUNT_PLACEHOLDER: 'your destination account',
+  REQUEST_ACCOUNT_TITLE: '2. Request a nano privacy account',
+  REQUEST_ACCOUNT_DESCRIPTION: 'Remember that nano privacy must be validated within 24hrs',
+  COUNTDOWN_TITLE: 'That\'s all!',
+  COUNTDOWN_DESCRIPTION: 'Now you have 24hrs to send your first nanos to this account.',
+  REQUEST: 'Request',
+  REQUEST_ERROR: 'Something\'s gone wrong! Please, refresh the page and try again',
+  CONTACT_TITLE: 'Feel free to send your comments and suggestions',
+  SEND: 'Send',
+  CONTACT_EMAIL_PLACEHOLDER: 'Your email',
+  CONTACT_MESSAGE_PLACEHOLDER: 'Your message',
+  ABOUT_US_TONI_BIO: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mi non erat facilisis ultrices. Integer ac vestibulum velit. In hac habitasse platea dictumst. Pellentesque sodales varius risus at lobortis. Duis dolor nisi, aliquet in suscipit id, porttitor a neque. Sed sem mauris, finibus eget varius non, tristique id mauris. Integer pellentesque lorem eget congue tincidunt',
+  ABOUT_US_CARLOS_BIO: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mi non erat facilisis ultrices. Integer ac vestibulum velit. In hac habitasse platea dictumst. Pellentesque sodales varius risus at lobortis. Duis dolor nisi, aliquet in suscipit id, porttitor a neque. Sed sem mauris, finibus eget varius non, tristique id mauris. Integer pellentesque lorem eget congue tincidunt'
 }
