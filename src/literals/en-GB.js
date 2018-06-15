1524346fd488d046d30a713c82280a52046d1d18
Carlos Villuendas <carlosvillu@gmail.com>
2018-06-15 21:29:56 +0200

    feat(literals): improve literals

--- src/literals/en-GB.js
+++ src/literals/en-GB.js
@@ -10,10 +10,10 @@ module.exports = {
   DESTINATION_ACCOUNT_PLACEHOLDER: 'your destination account',
   DESTINATION_ACCOUNT_INFO_TITLE: '',
   DESTINATION_ACCOUNT_INFO_DESCRIPTION:
-    'Make sure you own this account. because will be the destination accoun',
+    'Make sure you own this account. because it will be the destination account',
   REQUEST_ACCOUNT_TITLE: '2. Request a nano privacy account',
   REQUEST_ACCOUNT_DESCRIPTION:
-    'Remember that nano privacy must be validated within 24hrs',
+    'Remember that nano privacy must be validated within 24 hrs',
   COUNTDOWN_TITLE: "That's all!",
   COUNTDOWN_DESCRIPTION:
     'Now you have 24hrs to send your first nanos to this account.',
@@ -25,27 +25,27 @@ module.exports = {
   CONTACT_EMAIL_PLACEHOLDER: 'Your email',
   CONTACT_MESSAGE_PLACEHOLDER: 'Your message',
   ABOUT_US_TONI_BIO:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mi non erat facilisis ultrices. Integer ac vestibulum velit. In hac habitasse platea dictumst. Pellentesque sodales varius risus at lobortis. Duis dolor nisi, aliquet in suscipit id, porttitor a neque. Sed sem mauris, finibus eget varius non, tristique id mauris. Integer pellentesque lorem eget congue tincidunt',
+    "Tony is a seasoned backend developer, who has worked in top-end projects, both as developer and as lead manager. An early adopter of cryptocurrency, he's passionate about it and is interested in providing solutions to current issues of privacy and security. When he's not working on one of his cryptocurrency projects, you're likely to find him watching any of the Star Wars movies.",
   ABOUT_US_CARLOS_BIO:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mi non erat facilisis ultrices. Integer ac vestibulum velit. In hac habitasse platea dictumst. Pellentesque sodales varius risus at lobortis. Duis dolor nisi, aliquet in suscipit id, porttitor a neque. Sed sem mauris, finibus eget varius non, tristique id mauris. Integer pellentesque lorem eget congue tincidunt',
+    "Carlos is a full-stack developer with 12+ years experience in software development. He's been involved in several international web projects with millions of daily users. He likes all things JavaScript and has a passion for minimalistic, highly-scalable web architecture. Avid contributor to open-source projects and, in general, an enthusiast of a free Internet. Lately he's grown an interest in the challenges of transactions with cryptocurrency.",
   ACCOUNT_KO_TITLE: 'Pair account/token are not valid',
   ACCOUNT_KO_DESCRIPTION:
-    'DONT send nanos to this account, because your money never will end in your destination account.',
+    'DO NOT send nanos to this account, because your money never end in your destination account.',
   ACCOUNT_OK_TITLE: 'You own this account',
   ACCOUNT_OK_DESCRIPTION:
-    'All nanos send it to this account will endedn in your destination account',
-  HOW_IT_IS_WORKS: "How it's works",
-  WHAT_IT_IS_TITLE: "What it's ?",
+    'All nanos sent to this account will end in your destination account',
+  HOW_IT_IS_WORKS: 'How does it work?',
+  WHAT_IT_IS_TITLE: "What's PRIVACY NANO?",
   WHAT_IT_IS_DESCRIPTION:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mi non erat facilisis ultrices. Integer ac vestibulum velit. In hac habitasse platea dictumst. Pellentesque sodales varius risus at lobortis. Duis dolor nisi, aliquet in suscipit id, porttitor a neque. Sed sem mauris, finibus eget varius non, tristique id mauris. Integer pellentesque lorem eget congue tincidunt',
-  HOW_IT_IS_WORKS_TITLE: "How it's ?",
+  HOW_IT_IS_WORKS_TITLE: 'How does it work?',
   HOW_IT_IS_WORKS_DESCRIPTION:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at mi non erat facilisis ultrices. Integer ac vestibulum velit. In hac habitasse platea dictumst. Pellentesque sodales varius risus at lobortis. Duis dolor nisi, aliquet in suscipit id, porttitor a neque. Sed sem mauris, finibus eget varius non, tristique id mauris. Integer pellentesque lorem eget congue tincidunt',
   CLAIM: 'Make your Nano cryptocurrency transactions untraceable',
   FLASHMESSAGE_BETA:
-    'We are in close BETA. DONT send nanos without read carefull this',
+    'We are in closed BETA. Please, DO NOT send nanos without reading this document carefully',
   BETA_TITLE: 'We are in closed beta',
   BETA_DESCRIPTION:
-    'We are open to a small group of users. If you are not one of them please! dont use this services. We are in heavy development and you can lost your money. In any case, even you are one of our beta testers, please, please, please, DONT send us more the 2 nanos. Beauce we will only return this amount if any error happend.',
+    "We are opened to a small group of users (beta testers). If you are not one of them, please don't use this service. We are in the midst of heavy development, so your money could potentially be lost. Even if you are one of our beta testers, please, DO NOT make transactions of more than 2 nanos, which is the maximum amount we will return in case an error occurs.",
   READ_MORE: 'read more'
 }
