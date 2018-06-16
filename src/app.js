6379094e8c2002e8d9061725b57b6634355900a5
Carlos Villuendas <carlosvillu@gmail.com>
2018-06-16 20:50:09 +0200

    feat(META): new events erros listeners

--- src/app.js
+++ src/app.js
@@ -11,13 +11,29 @@ import withContext from '@s-ui/hoc/lib/withContext'
 import createClientContextFactoryParams from '@s-ui/react-initial-props/lib/createClientContextFactoryParams'
 import contextFactory from './contextFactory'
 
-import events, {GA_EVENTS} from './libs/events'
+import events, {GA_EVENTS, CATEGORIES} from './libs/events'
 
 import './styles/index.scss'
 
 events.subscribe(GA_EVENTS, ({category, action, label, value}) => {
   window.ga('send', 'event', category, action, label, value)
 })
+
+window.onerror = (message, file, line, col, error) => {
+  events.publish(GA_EVENTS, {
+    category: CATEGORIES.JS.GLOBAL.ERROR,
+    action: file,
+    label: message
+  })
+}
+
+window.addEventListener('unhandledrejection', event =>
+  events(GA_EVENTS, {
+    category: CATEGORIES.JS.PROMISE.UNHANDLE,
+    action: event.promise,
+    label: event.reason
+  })
+)
 
 contextFactory(createClientContextFactoryParams()).then(context => {
   match(
